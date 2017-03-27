c8ecc83446cb0f916273c1ff049b49a8c50c14e1
Matthew Vogee <mvogee@yahoo.com>
2017-03-27 15:39:27 -0700

    all working

--- src/output.c
+++ src/output.c
@@ -12,6 +12,29 @@
 
 #include "ft_ls.h"
 
+void	output_date(t_fileinfo *file)
+{
+	time_t	curtime;
+	char	*filetime;
+	char	*p2;
+	int 	time_or_year;
+
+	time_or_year = 0;
+	curtime = time(NULL);
+	filetime = ft_strsub(ctime(&file->st->st_mtime), 4, 6);
+	if (!filetime)
+		return ;
+	if (file->st->st_mtime > curtime || file->st->st_mtime + SIX_MONTHS < curtime)
+		p2 = ft_strsub(ctime(&file->st->st_mtime), 20, 4);
+	else
+		p2 = ft_strsub(ctime(&file->st->st_mtime), 11, 5);
+	ft_printf("%s %5s ", filetime, p2); // this needs formattings
+	if (filetime)
+		free(filetime);
+	if (p2)
+		free(p2);
+}
+
 void	output_info(t_fileinfo *files, t_options *options, t_outinfo_gen *oi)
 {
 	t_fileinfo *tmp;
@@ -32,16 +55,11 @@ void	output_info(t_fileinfo *files, t_options *options, t_outinfo_gen *oi)
 			ft_printf("%*llu ", oi->serial_min_wid ,tmp->st->st_ino);
 		if (options->option_l)
 		{ // need to take into account all the flags for special output as well.
-		//	output_rights(tmp, options, oi); // make this
-		//	output_links_or_sysnum(tmp, options, oi); // make this
-		//	output_users(tmp, options, oi); // make this
-		//	output_size(tmp, optoions, oi); // make this
-		//	output_date(tmp, options, oi); // make this
 			ft_printf("%s %*d ", tmp->rights, oi->links_min_wid, tmp->st->st_nlink);
 			// here figure out if you need to print system number things instead of links
 			ft_printf("%*s  %*s ",  oi->user_min_wid, tmp->owner_name, oi->group_min_wid, tmp->group_name);
 			ft_printf(" %*lld ", oi->file_size, tmp->st->st_size);
-			ft_printf("%s ", ctime(&files->st->st_mtime)); // this needs formattings
+			output_date(tmp);
 		}
 		ft_printf("%s", tmp->filename);
 		ft_printf("\n");
